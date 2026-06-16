a12661b1a2447d074abde1b334191798ac02ceef
Soha-Sayyad <soha.sayyad24@sakec.ac.in>
2026-06-16 16:21:26 +0530

    streamlit ui code

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import tempfile 
 st.title("Echo Bot")
 prompt = st.chat_input("Say something")
 if prompt:
     st.write(f"User: {prompt}")
 
+pdf_file = st.file_uploader("Upload a PDF", type=["pdf"])
+if pdf_file:
+    with tempfile.NamedTemporaryFile(mode="wb", suffix=".pdf") as temp:
+        temp.write(pdf_file.getvalue())
