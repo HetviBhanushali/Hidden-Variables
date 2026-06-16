e7ec9a86936208f8c02f7f91f685041388fecffa
Soha-Sayyad <soha.sayyad24@sakec.ac.in>
2026-06-16 15:15:04 +0530

    similaritiy score

--- vectorization.py
+++ vectorization.py
@@ -5,39 +5,46 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-HF_TOKEN = os.getenv("HF_TOKEN")
-login(HF_TOKEN)
 
-embeddings = HuggingFaceEmbeddings(model_name = "BAAI/bge-small-en-v1.5")
+HF_TOKEN = os.getenv("HF_TOKEN")
+login(token=HF_TOKEN)
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="BAAI/bge-small-en-v1.5"
+)
 
 vector_store = Chroma(
     collection_name="pdf-rag",
-    persist_directory = "./chroma_langchain_db",
-    embedding_function = embeddings,
+    persist_directory="./chroma_langchain_db",
+    embedding_function=embeddings,
 )
+
 print(f"Collection count: {vector_store._collection.count()}")
 
 while True:
-    user_prompt = input("Prompt (type 'q' to quit):")
-    if user_prompt.strip() == "q":
+    user_prompt = input("\nPrompt (type 'q' to quit): ")
+
+    if user_prompt.strip().lower() == "q":
         break
 
     results = vector_store.similarity_search_with_score(
         user_prompt,
-        k = 5
+        k=5
     )
-    score_result = []
-    for doc, score in results:
-        if score<1.0:
-            score_result.append((doc,score))
 
-    if not score_result:
+    filtered_results = [
+        (doc, score)
+        for doc, score in results
+        if score < 1.0
+    ]
+
+    if not filtered_results:
         print("No relevant information found in the PDF.")
         continue
 
-    for i, doc in enumerate(score_result, 1):
-        print(f"Result: {i}")
-        print(f"Source : {doc.metadata.get('filename')}")
-        print(f"Page : {doc.metadata.get('page')}")
+    for i, (doc, score) in enumerate(filtered_results, start=1):
+        print(f"\nResult {i}")
+        print(f"Score: {score:.4f}")
+        print(f"Source: {doc.metadata.get('filename', 'Unknown')}")
+        print(f"Page: {doc.metadata.get('page', 'Unknown')}")
         print(doc.page_content)
-        print(" ")
