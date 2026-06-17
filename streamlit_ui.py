c84ddb1ebc87533fe3378675b8c22b47844c99f6
Soha-Sayyad <soha.sayyad24@sakec.ac.in>
2026-06-17 11:53:17 +0530

    no of pages

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+from main import answer_question
 st.title("Echo bot")
 
 
@@ -14,3 +14,23 @@ prompt = st.chat_input("Ask a question about the PDF")
 
 if prompt:
     st.chat_message("user").write(prompt)
+
+    answer = answer_question(prompt) 
+    st.chat_message("assistant").write(answer)
+
+if uploaded_file:
+
+    from pypdf import PdfReader
+
+    pdf = PdfReader(uploaded_file)
+
+    text = ""
+
+    for page in pdf.pages:
+        page_text = page.extract_text()
+
+        if page_text:
+            text += page_text
+
+    st.write("Pages:", len(pdf.pages))
+    st.text(text[:3000])
