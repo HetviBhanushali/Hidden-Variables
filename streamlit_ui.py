8ef5f1a6b9d71b0a95fd2b1027b542ca24904228
vaishnavigundeti12 <vaishnavigundeti23@gmail.com>
2026-06-18 12:45:59 +0530

    initial commit

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -1,42 +1,68 @@
+import os
 import streamlit as st
-import tempfile
+import re
 
 from vector import build_index
 from main import answer_question
 
 st.title("PDF Q&A Bot")
 
+if "last_uploaded_file" not in st.session_state:
+    st.session_state.last_uploaded_file = None
+
+if "collection_name" not in st.session_state:
+    st.session_state.collection_name = None
+
 uploaded_file = st.sidebar.file_uploader(
     "Upload PDF",
     type=["pdf"]
 )
 
-if uploaded_file:
+if uploaded_file is not None:
 
-    with tempfile.NamedTemporaryFile(
-        delete=False,
-        suffix=".pdf"
-    ) as tmp:
+    # Always keep collection name available
+    pdf_name = os.path.splitext(
+        uploaded_file.name
+    )[0]
+    pdf_name = re.sub(
+    r"[^a-zA-Z0-9._-]",
+    "_",
+    pdf_name
+)
+    st.session_state.collection_name = pdf_name
 
-        tmp.write(uploaded_file.read())
-        pdf_path = tmp.name
+    if uploaded_file.name != st.session_state.last_uploaded_file:
 
-    build_index(pdf_path)
+        pdf_path = f"temp_{uploaded_file.name}"
 
-    st.success("PDF indexed successfully!")
+        with open(pdf_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
 
-prompt = st.chat_input("Ask a question")
+        num_chunks = build_index(
+            pdf_path,
+            pdf_name
+        )
+
+        st.session_state.last_uploaded_file = uploaded_file.name
+
+        st.success(
+            "pdf loaded successfully"
+        )
+
+prompt = st.chat_input(
+    "Ask a question"
+)
 
 if prompt:
 
-    st.chat_message("user").write(prompt)
+    if st.session_state.collection_name is None:
+        st.error("Please upload a PDF first.")
+    else:
+        st.chat_message("user").write(prompt)
 
-    answer = answer_question(prompt)
+        answer = answer_question(
+            prompt,
+            st.session_state.collection_name
+        )
 
-    st.chat_message("assistant").write(answer)
-
-if uploaded_file and "indexed" not in st.session_state:
-
-    build_index(pdf_path)
-
-    st.session_state.indexed = True
+        st.chat_message("assistant").write(answer)
