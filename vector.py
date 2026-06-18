c881996d1b0819ef9d0f99aa01f2980edc508fe2
snehachheda963 <sneha.chheda.963@gmail.com>
2026-06-18 11:46:46 +0530

    Sneha added vector

--- vector.py
+++ vector.py
@@ -2,12 +2,10 @@ from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-import shutil
-import os
 
 DB_PATH = "./chroma_langchain_db"
 
-def build_index(pdf_path):
+def build_index(pdf_path, collection_name):
 
     loader = PyPDFLoader(pdf_path)
     documents = loader.load()
@@ -26,8 +24,10 @@ def build_index(pdf_path):
     Chroma.from_documents(
         documents=chunks,
         embedding=embeddings,
-        collection_name="pdf-rag",
+        collection_name=collection_name,
         persist_directory=DB_PATH
     )
+
+    print("Created collection:", collection_name)
 
     return len(chunks)
