ae715dbe80bbe524a0904799766e140b7d6e1d2a
Soha-Sayyad <soha.sayyad24@sakec.ac.in>
2026-06-18 10:12:25 +0530

    collection_name from user

--- vector.py
+++ vector.py
@@ -2,22 +2,22 @@ from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-import shutil
+#import shutil
 import os
 
 DB_PATH = "./chroma_langchain_db"
 
-def build_index(pdf_path):
+def build_index(pdf_path,collection_name):
 
     # Delete old DB if it exists
-   # if os.path.exists(DB_PATH):
+    #if os.path.exists(DB_PATH):
     #    shutil.rmtree(DB_PATH)
 
-    # Load PDF
+   
     loader = PyPDFLoader(pdf_path)
     documents = loader.load()
 
-    # Split into chunks
+    
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200
@@ -25,16 +25,16 @@ def build_index(pdf_path):
 
     chunks = splitter.split_documents(documents)
 
-    # Embeddings
+    
     embeddings = HuggingFaceEmbeddings(
         model_name="BAAI/bge-small-en-v1.5"
     )
 
-    # Create Chroma DB
+    
     Chroma.from_documents(
         documents=chunks,
         embedding=embeddings,
-        collection_name="pdf-rag",
+        collection_name=collection_name,
         persist_directory=DB_PATH
     )
 
